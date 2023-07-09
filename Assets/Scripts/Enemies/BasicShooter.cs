afe790f11d1c073e5fd8f8dcb48699681337d8c1
Kervina <kervin.liu2002@hotmail.com>
2023-07-09 01:56:44 -0400

    shooter rework

--- Assets/Scripts/Enemies/BasicShooter.cs
+++ Assets/Scripts/Enemies/BasicShooter.cs
@@ -22,7 +22,7 @@ public class BasicShooter : Enemy {
 		}
 	}
 
-	public override void PlayerActivate() {
+    public override void PlayerActivate() {
 		SetTarget(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)));
 		ZoneActivate();
 	}
@@ -30,9 +30,21 @@ public class BasicShooter : Enemy {
 	public override void ZoneActivate() {
         isInteractable = false;
         GameObject bullet = Instantiate(bulletPrefab);
-		bullet.GetComponent<Bullet>().Setup(gameObject, unitVel);
-		myBullets.Add(bullet);
-		health = 0;
+
+        Vector2 leftVec = Quaternion.Euler(0f, 0f, -10) * unitVel;
+        GameObject bullet2 = Instantiate(bulletPrefab);
+
+        Vector2 rightVec = Quaternion.Euler(0f, 0f, 10) * unitVel;
+        GameObject bullet3 = Instantiate(bulletPrefab);
+
+        bullet.GetComponent<Bullet>().Setup(gameObject, unitVel, bullet2, bullet3);
+        myBullets.Add(bullet);
+        bullet2.GetComponent<Bullet>().Setup(gameObject, leftVec, bullet, bullet3);
+        myBullets.Add(bullet2);
+        bullet3.GetComponent<Bullet>().Setup(gameObject, rightVec, bullet, bullet2);
+        myBullets.Add(bullet3);
+
+        health = 0;
 	}
 
 	public override void CheckCollisions() {
@@ -42,8 +54,8 @@ public class BasicShooter : Enemy {
 				continue;
 			}
 
-			// usual collision stuff
-			if (collision.gameObject.tag == "Danger" || collision.gameObject.tag == "Player") {
+            // usual collision stuff
+            if (collision.gameObject.tag == "Danger" || collision.gameObject.tag == "Player") {
 				health -= 1;
 			} else if (isInteractable && collision.gameObject.tag == "Selection") {
 				isSelected = true;
--- Assets/Scripts/Enemies/Bullet.cs
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,24 +11,38 @@ public class Bullet : MonoBehaviour {
 	private GameObject parent;
 	private bool hit = false;
 
-	public void Setup(GameObject parent, Vector2 unitVel) {
+    [SerializeField]
+    protected ParticleSystem deathParticles;
+
+    // sibling bullets
+    GameObject sib1;
+    GameObject sib2;
+
+    public void Setup(GameObject parent, Vector2 unitVel, GameObject sib1, GameObject sib2) {
 		this.parent = parent;
 		this.unitVel = unitVel;
 		transform.position = (Vector2)parent.transform.position + unitVel * 0.25f;
-	}
+		this.sib1 = sib1;
+        this.sib2 = sib2;
+    }
 
-	void Update() {
+    void Update() {
 		transform.position += (Vector3)unitVel * speed * Time.deltaTime;
 		if (hit) {
-			Destroy(gameObject);
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+            Destroy(gameObject);
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D collision) {
-		// TODO: should bullets destroy other bullets?
+        // TODO: should bullets destroy other bullets?
+        if (collision.gameObject == sib1 || collision.gameObject == sib2)
+        {
+			return;
+        }
 
-		// don't hit zones or selection or scrambler field
-		if (collision.tag == "Zone" || collision.tag == "Selection" || collision.tag == "Scrambling") {
+        // don't hit zones or selection or scrambler field
+        if (collision.tag == "Zone" || collision.tag == "Selection" || collision.tag == "Scrambling") {
 			return;
 		}
 		// don't hit the enemy spawning the bullet
